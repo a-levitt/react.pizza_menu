4ee10efddd18a615aadf6348966dcd5dab4a4863
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-06 11:51:25 +0100

    Conditional rendering for open hours

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,19 @@
 
 function Footer() {
-    return <footer className='footer'>We are open 8am - 22pm CET. Current time: {new Date().toLocaleTimeString()}</footer>
+    const hour = new Date().getHours();
+    const openHour = 8;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    return (
+        <footer className='footer'>
+            <div className="order">
+                {isOpen && <p>We are open now!</p>}
+                <p>Work hours: {openHour}:00 - {closeHour}:00 CET.</p>
+                <button className="btn">Order</button>
+            </div>
+
+        </footer>
+    )
 }
 
 export default Footer;
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,11 +2,13 @@ import PizzaData from "./../data.js";
 import Pizza from "./Pizza.jsx";
 
 function Menu() {
+    const pizzas = PizzaData;
     return (
         <main className="menu">
             <h2>Our menu</h2>
+
             <ul className="pizzas">
-                {PizzaData.map(pizza =>
+                {pizzas.map(pizza =>
                     <Pizza pizzaObj={pizza} key={pizza.name}/>
                 )}
             </ul>
